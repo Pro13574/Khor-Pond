1fc449aa14bf75a15ff35cf30c115310aceb4cae
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-03-18 17:16:22 +0700

    add FishData.py

---
+++ FishData.py
@@ -0,0 +1,62 @@
+import random
+import math
+
+def randId(self):
+    digits = [i for i in range(0, 10)]
+    random_str= ""
+    for i in range(6):
+        index = math.floor(random.random() * 10)
+        random_str += str(digits[index])
+    return random_str
+
+
+def randCrowdTs():
+    return random.randint(5, 20)
+
+def randPheroTs():
+    return random.randint(30, 60)
+
+class FishData:
+    def __init__(self, genesis, parentId=None):
+        self.id = randId()
+        self.state = "in-pond"
+        self.status = "alive"
+        self.genesis = genesis
+        self.crowdTs = randCrowdTs()
+        self.pheromone = 0
+        self.pheromoneTs = randPheroTs()
+        self.lifetime = 60
+        self.parentId = parentId
+
+    def getId(self):
+        return self.id
+
+    def getState(self):
+        return self.state
+
+    def getStatus(self):
+        return self.status
+
+    def getGenesis(self):
+        return self.genesis
+
+    def getcrowdThreshold(self):
+        return self.crowdThreshold
+
+    def pheromone(self):
+        return self.pheromone
+
+    def pheromoneThresh(self):
+        return self.pheromoneThresh
+
+    def lifetime(self):
+        return self.lifetime
+
+    def parentId(self):
+        return self.parentId
+    
+    def __str__(self):
+        if self.parentId:
+            return self.id + "Genesis:" + self.genesis + " Parent:" + self.parentId + " Lifetime: " + str(self.lifetime)
+        else:
+            return self.id + "Genesis:" + self.genesis + " Lifetime: " + str(self.lifetime)
