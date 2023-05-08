70e4e18d9d53f16383f211d3a88f0ba4bdf378a0
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-09 01:29:59 +0700

    testrun add VIvisystemClient path

--- testrun.py
+++ testrun.py
@@ -2,10 +2,15 @@ from Pond import Pond
 
 from FishStore import FishStore, connect_to_redis
 
+from vivisystem.client import VivisystemClient
+
+POND_NAME = "Khor-pond"
+VIVISYSTEM_URL = "ws://localhost:5000"
+
 r = connect_to_redis()
 fishStore = FishStore(r)
-
-pond = Pond("Khor-pond", fishStore)
+client = VivisystemClient(VIVISYSTEM_URL, POND_NAME)
+pond = Pond("Khor-pond", fishStore, client)
 pond.run()
 
 # pond = Pond()
