7cfbf015e4e121be2cb4f96967cdeefa338bdd9a
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2023-05-09 04:12:11 +0700

    modify PondFrame

--- PondFrame.py
+++ PondFrame.py
@@ -10,18 +10,31 @@ class PondFrame(QGroupBox):
         self.vbox = QVBoxLayout()
         self.hbox = QHBoxLayout()
         self.vbox.addLayout(self.hbox)
+        self.name = QLabel()
+        self.totalFishes = QLabel()
+        self.pheromone = QLabel()
+        self.name.setStyleSheet(
+            "color: white; font-size: 16px; font-weight: 700;")
+        self.totalFishes.setStyleSheet(
+            "color: white; font-size: 16px; font-weight: 700;")
+        self.pheromone.setStyleSheet(
+            "color: white; font-size: 16px; font-weight: 700;")
         self.setLayout(self.vbox)
         self.addInfo(info)
 
     def addInfo(self, info):
         label = QLabel(self)
-
         self.addLabel(label)
-
-        self.addLabel(QLabel("Pond Name: " + str(info[0])))
-        self.addLabel(QLabel("Amount of Fishes: " + str(info[1])))
-        for i in info[2]:
-            self.addLabel(QLabel(str(i)))
+        self.name.setText("Pond Name: " + info[0])
+        self.totalFishes.setText("Fishes in the pond: " + str(info[1]))
+        self.pheromone.setText("Pheromone: " + str(info[2]))
+        self.addLabel(self.name)
+        self.addLabel(self.totalFishes)
+        self.addLabel(self.pheromone)
 
     def addLabel(self, widget):
         self.vbox.addWidget(widget)
+
+    def updateInfo(self, info):
+        self.totalFishes.setText("Fishes in the pond: " + str(info[1]))
+        self.pheromone.setText("Pheromone: " + str(info[2]))
